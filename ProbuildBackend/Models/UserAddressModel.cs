0e4d80b65fd2d7d39eeb039b797cc2cf0917608e
Johan <Johandutoit139@gmail.com>
2025-11-06 14:30:05 +0200

    Model fix on address

--- ProbuildBackend/Models/UserAddressModel.cs
+++ ProbuildBackend/Models/UserAddressModel.cs
@@ -8,24 +8,24 @@ namespace ProbuildBackend.Models
     public class UserAddressModel
     {
         public long Id { get; set; }
-        public string StreetNumber { get; set; }
-        public string StreetName { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
+        public string? StreetNumber { get; set; }
+        public string? StreetName { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
 
-        public bool Deleted { get; set; }
-        public string CountryCode { get; set; }
+        public bool? Deleted { get; set; }
+        public string? CountryCode { get; set; }
 
         public decimal? Latitude { get; set; }
         public decimal? Longitude { get; set; }
         [JsonIgnore]
-        public Point Location { get; set; }
-        public string FormattedAddress { get; set; }
-        public string GooglePlaceId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public Point? Location { get; set; }
+        public string? FormattedAddress { get; set; }
+        public string? GooglePlaceId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
 
         public string? AddressType { get; set; }
 
